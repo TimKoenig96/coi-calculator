f4b5fa3a2649d401e166e6e767421ec2d5dd963e
Tim König <137522127+TimKoenig96@users.noreply.github.com>
2024-11-17 20:22:02 +0100

    edit: Moved user notification class to own file

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
+// #region | Imports
+import { UserNotification } from "./user_notification.js";
+// #endregion
+
+
 // #region | Variables
 let worker;
-let current_calculator = "production";
 let research_open = false;
 let recipes_open = false;
 // #endregion
@@ -41,54 +45,6 @@ const power_calculator_canvas = document.getElementById("power_calculator_canvas
 
 // Notifications container
 const notifs_container = document.getElementById("notifs_container");
-// #endregion
-
-
-// #region | Classes
-class UserNotification {
-	#valid_styles = ["default", "success", "warning", "error"];
-
-	/**
-	 * Create a new notification
-	 * @param {String} header Message header
-	 * @param {String} message Message to display
-	 * @param {("default"|"success"|"warning"|"error")?} style Message style
-	 * @param {Number?} timer Message auto-delete after specified ms. 0 for permanent.
-	 */
-	constructor(header, message, style = "default", timer = 0) {
-		this.element;
-		this.timeout;
-
-		// Check correct style use
-		if (!this.#valid_styles.includes(style)) style = "default";
-
-		// Create the element
-		this.#createNotif(header, message, style);
-
-		// If element has timer, set timeout
-		if (timer) this.timeout = setTimeout(this.#removeNotif.bind(this), timer);
-
-		// Show notification
-		this.#showNotif();
-	}
-
-	#createNotif(header, message, style) {
-		this.element = document.createElement("div");
-		if (style) this.element.classList.add(style);
-		this.element.innerHTML = `<h1>${header}</h1><p>${message}</p><br><a href="">Close message</a>`;
-		this.element.children[3].addEventListener("click", this.#removeNotif.bind(this));
-	}
-
-	#showNotif() {
-		notifs_container.prepend(this.element);
-	}
-
-	#removeNotif(e) {
-		e?.preventDefault();
-		this.element.remove();
-		clearTimeout(this.timeout);
-	}
-}
 // #endregion
 
 
