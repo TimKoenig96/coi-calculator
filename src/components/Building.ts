c27d313230ab516f55acaaa13d99c498ba77490c
Tim König <137522127+TimKoenig96@users.noreply.github.com>
2024-11-21 21:35:11 +0100

    breaking: Building class and related enums change

--- src/components/Building.ts
+++ src/components/Building.ts
@@ -1,7 +1,50 @@
-import { Items } from "./Item";
+enum BuildingID {
 
-export class Building {
+	// #region | 1xx General Machines
+	ASSEMBLY_I = 101,
+	ASSEMBLY_II = 102,
+	ASSEMBLY_III = 103,
+	ASSEMBLY_ROBOTIC = 104
+	// #endregion
+
+	// #region | 2xx Water Extraction & Processing
+	// #endregion
+
+	// #region | 3xx Food Production
+	// #endregion
+
+	// #region | 4xx Metallurgy & Smelting
+	// #endregion
+
+	// #region | 5xx Power Production
+	// #endregion
+
+	// #region | 6xx Crude Oil Refining
+	// #endregion
+
+	// #region | 7xx Waste Management
+	// #endregion
+
+	// #region | 9xx Buildings
+	// #endregion
+
+	// #region | 10xx Housing & Services
+	// #endregion
+}
+
+class Building {
+	readonly id: BuildingID;
+	readonly name: string;
+	readonly workers: number;
+	readonly electricity: number;
+	readonly computing: number;
+	readonly maintenance_1: number;
+	readonly maintenance_2: number;
+	readonly maintenance_3: number;
+	readonly build_cost: ItemQuantity[];
+
 	constructor(
+		id: BuildingID,
 		name: string,
 		workers: number,
 		electricity: number,
@@ -9,35 +52,50 @@ export class Building {
 		maintenance_1: number,
 		maintenance_2: number,
 		maintenance_3: number,
-		build_cost: Partial<Record<keyof typeof Items, number>>
+		build_cost: ItemQuantity[]
 	) {
+		this.id = id;
+		this.name = name;
+		this.workers = workers;
+		this.electricity = electricity;
+		this.computing = computing;
+		this.maintenance_1 = maintenance_1;
+		this.maintenance_2 = maintenance_2;
+		this.maintenance_3 = maintenance_3;
+		this.build_cost = build_cost;
 	}
 }
 
-export class Buildings {
+const buildings: Record<BuildingID, Building> = {
 
-	// #region | General Machines
-	static ASSEMBLY_I: Building = new Building("Assembly I", 4, 0.04, 0, 0, 0, 0, {CONSTRUCTION_PARTS: 25})
+	// #region | 1xx General Machines
+	[BuildingID.ASSEMBLY_I]: new Building(BuildingID.ASSEMBLY_I, "Assembly I", 4, 0.04, 0, 0, 0, 0, [{item: items[ItemID.CONSTRUCTION_PARTS], quantity: 25}]),
+	[BuildingID.ASSEMBLY_II]: new Building(BuildingID.ASSEMBLY_II, "Assembly II", 6, 0.08, 0, 2, 0, 0, [{item: items[ItemID.CONSTRUCTION_PARTS_II], quantity: 30}]),
+	[BuildingID.ASSEMBLY_III]: new Building(BuildingID.ASSEMBLY_III, "Assembly III", 8, 0.15, 0, 4, 0, 0, [{item: items[ItemID.CONSTRUCTION_PARTS_III], quantity: 40}]),
+	[BuildingID.ASSEMBLY_ROBOTIC]: new Building(BuildingID.ASSEMBLY_ROBOTIC, "Assembly (Robotic)", 2, 0.25, 4, 0, 4, 0, [{item: items[ItemID.CONSTRUCTION_PARTS_IV], quantity: 40}])
 	// #endregion
 
-	// #region | Water Extraction & Processing
+	// #region | 2xx Water Extraction & Processing
 	// #endregion
 
-	// #region | Food Production
+	// #region | 3xx Food Production
 	// #endregion
 
-	// #region | Metallurgy & Smelting
+	// #region | 4xx Metallurgy & Smelting
 	// #endregion
 
-	// #region | Power Production
+	// #region | 5xx Power Production
 	// #endregion
 
-	// #region | Crude Oil Refining
+	// #region | 6xx Crude Oil Refining
 	// #endregion
 
-	// #region | Waste Management
+	// #region | 7xx Waste Management
 	// #endregion
 
-	// #region | Buildings
+	// #region | 9xx Buildings
 	// #endregion
-}
+
+	// #region | 10xx Housing & Services
+	// #endregion
+};
