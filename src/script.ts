8abe628d6ff0f849e55bdca5815a207f10fdca82
Tim König <137522127+TimKoenig96@users.noreply.github.com>
2024-11-26 22:42:36 +0100

    add: Loading and parsing settings
    Loading and parsing settings via URL search parameters (if found) or local storage (if URL search parameters weren't found or if they failed).
    
    Also prompting user before clearing either.

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,6 @@
+import { NotificationType, UserNotification } from "./components/UserNotification";
+import { UserPrompt } from "./components/UserPrompt";
+
 // #region | Variables
 let search_params_applied: boolean = false; // Prevents saving edits to local storage
 // #endregion
@@ -21,9 +24,14 @@ interface ParsedSettings {
 
 
 // #region | Functions
-// TODO: Attempt to apply all search parameters
-function applyParsedSettings(parameters: ParsedSettings): boolean {
-	return false;
+// TODO: Attempt to apply all parsed settings
+function applyParsedSettings(settings: ParsedSettings | null): boolean {
+
+	// No parameters provided, return no success
+	if (settings === null) return false;
+
+	// FIXME: Just returning successful if settings are provided at all for now.
+	return true;
 }
 
 // Attempt to parse and return only the required settings
@@ -79,6 +87,16 @@ function parseLoadedSettings(settings: LoadedSettings | null): ParsedSettings | 
 	}
 }
 
+// Load setting strings from local storage
+function loadSettingsFromLocalStorage(): LoadedSettings | null {
+	const research: string | null = window.localStorage.getItem("research");
+	const recipes: string | null = window.localStorage.getItem("recipes");
+	const inputs: string | null = window.localStorage.getItem("inputs");
+	const outputs: string | null = window.localStorage.getItem("outputs");
+
+	return { research, recipes, inputs, outputs };
+}
+
 // Load settings from search parameters
 function loadSettingsFromSearchParams(): LoadedSettings | null {
 	const params: URLSearchParams = new URLSearchParams(window.location.search);
@@ -91,42 +109,132 @@ function loadSettingsFromSearchParams(): LoadedSettings | null {
 	return { research, recipes, inputs, outputs };
 }
 
-// Request to load, parse and apply search parameters
-function loadParseApplySearchParams(): boolean | null {
+// Attempt to load, parse and apply settings from local storage
+async function loadParseApplyLocalStorage(): Promise<boolean | null> {
+
+	// Load settings via local storage
+	const settings: LoadedSettings | null = loadSettingsFromLocalStorage();
+
+	// Attempt to parse settings
+	const parsed_settings: ParsedSettings | null = parseLoadedSettings(settings);
+
+	// Attempt to apply parsed settings
+	const success: boolean = applyParsedSettings(parsed_settings);
+
+	// Unsuccessful
+	if (!success) {
+
+		// Ask user if they want to reset
+		const reset: boolean = await new UserPrompt(
+			"Invalid parameters in Local Storage",
+			`The data in your browsers local storage seems to be invalid.<br><br>
+
+			Do you want to clear it? This will reset all of your selected researches and recipes.`,
+			NotificationType.ERROR,
+			[
+				{ label: "Yes, reset", retval: true },
+				{ label: "No, abort", retval: false }
+			]
+		).prompt();
+
+		// User wants to reset local storage
+		if (reset) {
+
+			// Clear local storage
+			window.localStorage.clear();
+
+			// Reload window
+			window.location.reload();
+
+			// Don't think I have to return at this point
+			return false;
+		}
+
+		// User wants to abort and keep local storage
+		else {
+
+			// Show abort success
+			new UserNotification(
+				"Invalid parameters in Local Storage",
+				`Okay, loading has been aborted.<br><br>
+
+				If this keeps happening, please do not hesitate to <a href="" target="_blank">create
+				an issue on GitHub</a>.<br><br>
+
+				Click <a href="#" onclick="navigator.clipboard.writeText(JSON.stringify(window.localStorage));">
+				here</a> to copy the local storage and provide it when creating an issue.`,
+				NotificationType.DEFAULT,
+				0,
+				false
+			);
+
+			// Return null indicating aborting the init process
+			return null;
+		}
+	}
+
+	// Successfully applied valid parameters
+	search_params_applied = true;
+	return true;
+}
+
+// Attempt to load, parse and apply settings from search parameters
+async function loadParseApplySearchParams(): Promise<boolean | null> {
 
 	// Load settings via search parameters
 	const settings: LoadedSettings | null = loadSettingsFromSearchParams();
 
 	// Attempt to parse settings
-	const parameters: ParsedSettings | null = parseLoadedSettings(settings);
+	const parsed_settings: ParsedSettings | null = parseLoadedSettings(settings);
 
-	// Invalid parameters
-	if (!parameters) {
+	// Attempt to apply parsed settings
+	const success: boolean = applyParsedSettings(parsed_settings);
 
-		// TODO: Ask user if they want to continue.
-		const continue_init: boolean = false;
-
-		// User wants to continue, indicate no success
-		if (continue_init) return false;
-
-		// User wants to abort, indicate error
-		else return null;
-	}
-
-	// Parameters are valid, attempt applying them
-	const success: boolean = applyParsedSettings(parameters);
-
-	// Unsuccessful at applying parsed settings
+	// Unsuccessful
 	if (!success) {
 
-		// TODO: Ask user if they want to continue.
-		const continue_init: boolean = false;
+		// Ask user if they want to continue
+		const continue_init: boolean = await new UserPrompt(
+			"Invalid Calculation in URL",
+			`The calculation specified in the URL after the question mark seems to
+			be invalid. You can continue without it, losing that calculation, or try again.`,
+			NotificationType.ERROR,
+			[
+				{ label: "Continue", retval: true },
+				{ label: "Try again", retval: false }
+			]
+		).prompt();
 
-		// User wants to continue, indicate no success
-		if (continue_init) return false;
+		// User wants to continue
+		if (continue_init) {
 
-		// User wants to abort, indicate error
-		else return null;
+			// Clear all search parameters and push the new state to history
+			const url = new URL(window.location.href);
+			url.search = "";
+			history.pushState(null, "", url.toString());
+
+			// Indicate no success
+			return false;
+		}
+
+		// User wants to abort
+		else {
+
+			// Show abort success
+			new UserNotification(
+				"Invalid Calculation in URL",
+				`Okay, loading has been aborted. You can edit the URL and try again.<br><br>
+
+				If this keeps happening, please do not hesitate to <a href="" target="_blank">create
+				an issue on GitHub</a>. Provide me the link and I'll see what's going on.`,
+				NotificationType.DEFAULT,
+				0,
+				false
+			);
+
+			// Return null indicating aborting the init process
+			return null;
+		}
 	}
 
 	// Successfully applied valid parameters
@@ -135,14 +243,14 @@ function loadParseApplySearchParams(): boolean | null {
 }
 
 // Initialize script
-function init() {
+async function init() {
 	let apply_success: boolean | null = null;
 
 	// If search parameters are present
 	if (window.location.search) {
 
-		// Attempt loading, parsing and applying via search parameters
-		apply_success = loadParseApplySearchParams();
+		// Attempt to load, parse and apply settings from search parameters
+		apply_success = await loadParseApplySearchParams();
 
 		// Error happened, stop requested
 		if (apply_success === null) return;
@@ -151,8 +259,14 @@ function init() {
 	// If search parameters weren't applied
 	if (!search_params_applied) {
 
-		// TODO: Attempt loading, parsing and applying via local storage
+		// Attempt to load, parse and apply settings from local storage
+		apply_success = await loadParseApplyLocalStorage();
 	}
+
+	// Nothing was successful
+	if (apply_success !== true) return;
+
+	// TODO: Think about what needs to happen next
 }
 // #endregion
 
