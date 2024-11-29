8afaa2f06b18e1f303ffa4918b3205723acccd64
Tim König <137522127+TimKoenig96@users.noreply.github.com>
2024-11-29 11:47:41 +0100

    edit: Added item categories

--- src/components/Item.ts
+++ src/components/Item.ts
@@ -4,20 +4,35 @@ export enum ItemID {
 	EXAMPLE_ITEM_3 = 3
 }
 
+enum ItemCategory {
+	NATURAL_RESOURCE = 0,
+	SEMI_PROCESSED_RESOURCE = 1,
+	CRAFTED_MATERIAL = 2,
+	FOOD_AND_MEDICAL = 3,
+	PETROCHEMICAL = 4,
+	POWER_AND_NUCLEAR = 5,
+	WASTE_AND_POLLUTION = 6
+}
+
 export type ItemCount = Partial<{
 	[key in ItemID]: number
 }>;
 
 class Item {
 	public readonly name: string;
+	private readonly category: ItemCategory;
 
-	constructor(name: string) {
+	constructor(
+		name: string,
+		category: ItemCategory
+	) {
 		this.name = name;
+		this.category = category;
 	}
 }
 
 const items: Record<ItemID, Item> = {
-	[ItemID.EXAMPLE_ITEM_1]: new Item("Example Item 1"),
-	[ItemID.EXAMPLE_ITEM_2]: new Item("Example Item 2"),
-	[ItemID.EXAMPLE_ITEM_3]: new Item("Example Item 3")
+	[ItemID.EXAMPLE_ITEM_1]: new Item("Example Item 1", ItemCategory.NATURAL_RESOURCE),
+	[ItemID.EXAMPLE_ITEM_2]: new Item("Example Item 2", ItemCategory.NATURAL_RESOURCE),
+	[ItemID.EXAMPLE_ITEM_3]: new Item("Example Item 3", ItemCategory.CRAFTED_MATERIAL)
 };
