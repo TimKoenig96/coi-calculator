87f535a203d0e18ceafa6f5845b530b5e9518fd6
Tim König <137522127+TimKoenig96@users.noreply.github.com>
2024-11-29 11:14:47 +0100

    edit: ItemCount as building cost

--- src/components/Building.ts
+++ src/components/Building.ts
@@ -1,3 +1,5 @@
+import { ItemCount, ItemID } from "./Item";
+
 export enum BuildingID {
 	EXAMPLE_BUILDING_1 = 1,
 	EXAMPLE_BUILDING_2 = 2,
@@ -6,19 +8,22 @@ export enum BuildingID {
 
 class Building {
 	public readonly name: string;
+	public readonly build_cost: ItemCount;
 	public unlocked: boolean;
 
 	constructor(
 		name: string,
+		build_cost: ItemCount,
 		unlocked: boolean = false
 	) {
 		this.name = name;
+		this.build_cost = build_cost;
 		this.unlocked = unlocked;
 	}
 }
 
 export const buildings: Record<BuildingID, Building> = {
-	[BuildingID.EXAMPLE_BUILDING_1]: new Building("Example Building 1"),
-	[BuildingID.EXAMPLE_BUILDING_2]: new Building("Example Building 2"),
-	[BuildingID.EXAMPLE_BUILDING_3]: new Building("Example Building 3")
+	[BuildingID.EXAMPLE_BUILDING_1]: new Building("Example Building 1", {[ItemID.EXAMPLE_ITEM_3]: 10}),
+	[BuildingID.EXAMPLE_BUILDING_2]: new Building("Example Building 2", {[ItemID.EXAMPLE_ITEM_3]: 20}),
+	[BuildingID.EXAMPLE_BUILDING_3]: new Building("Example Building 3", {[ItemID.EXAMPLE_ITEM_3]: 40})
 };
